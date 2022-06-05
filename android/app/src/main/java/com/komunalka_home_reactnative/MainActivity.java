9ccf3363d93c91a171fd1abb179e2923f3fe77f2
Yvgen Stolbovyii <stolbovyii@mydigicode.com>
2022-06-05 23:40:02 +0200

    add lunch screen android

--- android/app/src/main/java/com/komunalka_home_reactnative/MainActivity.java
+++ android/app/src/main/java/com/komunalka_home_reactnative/MainActivity.java
@@ -1,6 +1,7 @@
 package com.komunalka_home_reactnative;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +13,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "komunalka_home_reactnative";
   }
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(null);
+    }
 }
