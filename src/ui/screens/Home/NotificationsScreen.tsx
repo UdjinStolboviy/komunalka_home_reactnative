67973230cb9ae649c0af157ce6aaf35c541a1f6b
Yvgen Stolbovyii <stolbovyii@mydigicode.com>
2022-08-02 18:37:22 +0200

    add notific

--- src/ui/screens/Home/NotificationsScreen.tsx
+++ src/ui/screens/Home/NotificationsScreen.tsx
@@ -8,7 +8,7 @@ import {
 } from 'app/utils/check-notification';
 import moment from 'moment';
 
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {
   View,
   Text,
@@ -21,11 +21,19 @@ import {
   IFlatItemNotification,
 } from '../notification-component/FlatItemNotification';
 import NotificationCalendarView from '../notification-component/NotificationCalendar';
+import notifee, {
+  AndroidNotificationSetting,
+  EventType,
+  RepeatFrequency,
+  TimestampTrigger,
+  TriggerType,
+} from '@notifee/react-native';
 
 import BackgroundService from 'react-native-background-actions';
-import { startTaskAction, stopTaskAction } from 'app/services/background-task/background.task.service';
-
-
+import {
+  startTaskAction,
+  stopTaskAction,
+} from 'app/services/background-task/background.task.service';
 
 export const NotificationsScreen = (props: any) => {
   const app: IAppCoreService = useAppInjection();
@@ -42,8 +50,6 @@ export const NotificationsScreen = (props: any) => {
 
   const datesNow = moment(new Date()).format('YYYY-MM');
   const dayNow = moment(new Date()).format('DD');
-
- 
 
   const dateNowSettlement = datesSettlement.map(date => {
     if (Number(date) >= 29) {
@@ -63,6 +69,75 @@ export const NotificationsScreen = (props: any) => {
     ...dateNowSettlement,
     ...datesSettlementNext(1, dateNowSettlement),
   ];
+
+  const checkNextDate = (): number => {
+    const dateNow = Date.parse(new Date());
+    const dateNext = datesSettlementNextMonth.find(item => {
+      if (Date.parse(item) > dateNow) {
+        return item;
+      }
+    });
+    return dateNext ? Date.parse(dateNext) : 17670;
+  };
+
+  useEffect(() => {
+    showNotification();
+    checkNextDate();
+  }, []);
+
+  const showNotification = async () => {
+    const settings = await notifee.getNotificationSettings();
+    if (settings.android.alarm == AndroidNotificationSetting.ENABLED) {
+      await notifee.requestPermission();
+
+      // Create a channel (required for Android)
+      const channelId = await notifee.createChannel({
+        id: 'defaultewrwerwer',
+        name: 'Default Channel',
+      });
+      //Create timestamp trigger
+      const trigger: TimestampTrigger = {
+        type: TriggerType.TIMESTAMP,
+        timestamp: checkNextDate(), // fire in 3 hours
+        repeatFrequency: RepeatFrequency.HOURLY, // repeat once a week
+        alarmManager: {
+          allowWhileIdle: true,
+        },
+      };
+      await notifee.createTriggerNotification(
+        {
+          title: 'БУДЬ ЛАСКА ЗНІМІТЬ ДАННІ',
+          body: 'Сьогодні треба зняти данні з квртири!',
+          android: {
+            channelId: channelId,
+            pressAction: {
+              id: 'default',
+            },
+          },
+        },
+        trigger,
+      );
+    } else {
+      // Show some user information to educate them on what exact alarm permission is,
+      // and why it is necessary for your app functionality, then send them to system preferences:
+      await notifee.openAlarmPermissionSettings();
+    }
+  };
+
+  notifee.onBackgroundEvent(async ({type}) => {
+    const initialNotification = await notifee.getInitialNotification();
+    // Check if the user pressed the "Mark as read" action
+    if (initialNotification) {
+      const notification = initialNotification.notification;
+      const pressAction = initialNotification.pressAction;
+      showNotification();
+      if (notification.id) {
+        // The user pressed the "Mark as read" action
+        await notifee.cancelNotification(notification.id);
+      }
+    }
+  });
+
   const renderNotificationList = () => {
     return flat.map((item, index) => {
       if (dayNow === item.settlementDay || Number(item.settlementDay) >= 29) {
